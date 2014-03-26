d78ef759b5b7dc2ddd6c6c7e44b80cd35a8eb6b6
Tomáš Livora <livoratom@gmail.com>
2014-03-26 22:00:18 +0100

    Improve animation when moving between screens

--- jbpm-mobile-showcase/src/main/java/org/kie/mobile/client/PhoneAnimationMapper.java
+++ jbpm-mobile-showcase/src/main/java/org/kie/mobile/client/PhoneAnimationMapper.java
@@ -19,6 +19,11 @@ import com.google.gwt.place.shared.Place;
 import com.googlecode.mgwt.mvp.client.Animation;
 import com.googlecode.mgwt.mvp.client.AnimationMapper;
 import javax.enterprise.context.ApplicationScoped;
+import org.kie.mobile.client.home.HomePlace;
+import org.kie.mobile.ht.client.newtask.NewTaskPlace;
+import org.kie.mobile.ht.client.taskdetails.TaskDetailsPlace;
+import org.kie.mobile.ht.client.tasklist.TaskListPlace;
+import org.kie.mobile.pr.client.definition.list.ProcessDefinitionListPlace;
 
 /**
  *
@@ -29,38 +34,38 @@ public class PhoneAnimationMapper implements AnimationMapper {
 
     @Override
     public Animation getAnimation(Place oldPlace, Place newPlace) {
-//        // home page - tasks list
-//        if (oldPlace instanceof HomePlace && newPlace instanceof TaskListPlace) {
-//            return Animation.SLIDE;
-//        }
-//        if (oldPlace instanceof TaskListPlace && newPlace instanceof HomePlace) {
-//            return Animation.SLIDE_REVERSE;
-//        }
-//        
-//        // home page - process definitions list
-//        if (oldPlace instanceof HomePlace && newPlace instanceof ProcessDefinitionListPlace) {
-//            return Animation.SLIDE;
-//        }
-//        if (oldPlace instanceof ProcessDefinitionListPlace && newPlace instanceof HomePlace) {
-//            return Animation.SLIDE_REVERSE;
-//        }
-//        
-//        // tasks list - new task page
-//        if (oldPlace instanceof TaskListPlace && newPlace instanceof NewTaskPlace) {
-//            return Animation.SLIDE;
-//        }
-//        if (oldPlace instanceof NewTaskPlace && newPlace instanceof TaskListPlace) {
-//            return Animation.SLIDE_REVERSE;
-//        }
-//        
-//        // tasks list - task details page
-//        if (oldPlace instanceof TaskListPlace && newPlace instanceof TaskDetailsPlace) {
-//            return Animation.SLIDE;
-//        }
-//        if (oldPlace instanceof TaskDetailsPlace && newPlace instanceof TaskListPlace) {
-//            return Animation.SLIDE_REVERSE;
-//        }
-        
+        // home page - tasks list
+        if (oldPlace instanceof HomePlace && newPlace instanceof TaskListPlace) {
+            return Animation.SLIDE;
+        }
+        if (oldPlace instanceof TaskListPlace && newPlace instanceof HomePlace) {
+            return Animation.SLIDE_REVERSE;
+        }
+
+        // home page - process definitions list
+        if (oldPlace instanceof HomePlace && newPlace instanceof ProcessDefinitionListPlace) {
+            return Animation.SLIDE;
+        }
+        if (oldPlace instanceof ProcessDefinitionListPlace && newPlace instanceof HomePlace) {
+            return Animation.SLIDE_REVERSE;
+        }
+
+        // tasks list - new task page
+        if (oldPlace instanceof TaskListPlace && newPlace instanceof NewTaskPlace) {
+            return Animation.SLIDE;
+        }
+        if (oldPlace instanceof NewTaskPlace && newPlace instanceof TaskListPlace) {
+            return Animation.SLIDE_REVERSE;
+        }
+
+        // tasks list - task details page
+        if (oldPlace instanceof TaskListPlace && newPlace instanceof TaskDetailsPlace) {
+            return Animation.SLIDE;
+        }
+        if (oldPlace instanceof TaskDetailsPlace && newPlace instanceof TaskListPlace) {
+            return Animation.SLIDE_REVERSE;
+        }
+
         // other
         return Animation.SLIDE;
     }
